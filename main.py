6d4f9820d9e7b0bbe7ebb8d91c14dc42c770ea2c
the Tim Ridenour <theTimRidenour@gmail.com>
2018-04-14 15:24:04 -0500

    added login function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://blogz:password@localhost:8889/blogz"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -14,10 +14,23 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(5000))
     date = db.Column(db.DateTime, default=datetime.now())
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
@@ -61,6 +74,42 @@ def add_post():
     post_id = Blog.query.all()
 
     return redirect("/blog?id=" + str(post_id[-1].id))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    
+
+    return render_template("signup.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not User.query.filter_by(username = username).first():
+            return render_template("login.html", error_username=True)
+
+        user = User.query.filter_by(username = username).first()
+        if not password == user.password:
+            return render_template("login.html", error_password=True)
+
+        session['username'] = username
+        flash("Logged in")
+        return redirect("/newpost")
+
+    return render_template("login.html")
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return redirect("/blog")
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    del session['username']
+    return redirect("/blog")
 
 if __name__ == "__main__":
     app.run()
